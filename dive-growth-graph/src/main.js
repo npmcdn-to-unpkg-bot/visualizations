77946e8fdef0d2baae80bbb74fda539b25a6775e
Hayden Wagner <haydenblainewagner@gmail.com>
2016-08-07 13:58:08 -0400

    Removes systemjs files and edits to simplify and match new format

--- dive-growth-graph/src/main.js
+++ dive-growth-graph/src/main.js
@@ -2,7 +2,6 @@
 // import {Person} from './person';
 // let person = new Person();
 // console.log(person.name);
-
 //data
 var sData = [
     {
@@ -19,80 +18,54 @@ var sData = [
         employees: 6,
         office: 'Grocery Store'
     }
-]
-
-
+];
 var vData = [];
 //end data
-
-
-
 //classes (should be able to use in dive-vis repo
-class FullSvg {
-    element: number;
-    drawPadding: number;
-    dimensions: number[];
-    drawDimensions: number[];
-
-
-    constructor(public container){
+var FullSvg = (function () {
+    function FullSvg(container) {
+        this.container = container;
         //noinspection TypeScriptUnresolvedVariable
         this.element = d3.select(container).append('svg');
         this.container = this.element.node().parentNode;
         console.log(this.container);
         console.log(this.container.clientWidth);
-
         this.drawPadding = 20;
         this.setDimensions();
     }
-
-    setDimensions(){
+    FullSvg.prototype.setDimensions = function () {
         this.dimensions = this.getContainerDimensions();
         this.element
             .style('width', this.dimensions[0])
             .style('height', this.dimensions[1]);
-
         this.computeDrawDimensions();
-    }
-
-    getContainerDimensions(){
+    };
+    FullSvg.prototype.getContainerDimensions = function () {
         var dimensions = [];
         dimensions.push(this.container.clientWidth);
         dimensions.push(this.container.clientHeight);
         console.log(dimensions);
-
         return dimensions;
-    }
-
-    computeDrawDimensions(){
+    };
+    FullSvg.prototype.computeDrawDimensions = function () {
         var padding = this.drawPadding;
-        this.drawDimensions = _.map(this.dimensions, function(x){return x - (padding * 2);});
-    }
-
-}
-
+        this.drawDimensions = _.map(this.dimensions, function (x) { return x - (padding * 2); });
+    };
+    return FullSvg;
+}());
 var svg = new FullSvg('body');
 console.log(svg);
-
-
 //make line scales
 var lineScaleX = d3.scaleLinear()
     .domain([2012, 2016])
     .range([svg.drawPadding, svg.drawDimensions[0]]);
-
 var lineScaleY = d3.scaleLinear()
-    .domain([0, d3.max(sData, function(d){return d.year;})])
-
-
+    .domain([0, d3.max(sData, function (d) { return d.year; })]);
 //make other scales
 var columnScale = d3.scaleLinear()
-    .domain([0,2])
+    .domain([0, 2])
     .range([svg.drawPadding, svg.drawDimensions[0]]);
-
 var publicationScaleVertical = d3.scaleLinear()
-    .domain([0,10])
+    .domain([0, 10])
     .range([svg.drawPadding, svg.drawDimensions[1]]);
-
-
-
-
+//# sourceMappingURL=main.js.map
